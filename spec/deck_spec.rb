fe911f4b8132b2459edf512bdddc3853120da333
Ian Lyell <ian.lyell@yahoo.com>
2023-05-08 20:56:32 -0500

    Add test to deck_spec

---
+++ spec/deck_spec.rb
@@ -0,0 +1,36 @@
+require "./lib/card"
+require "./lib/deck"
+
+RSpec.describe do
+
+    it "stores cards" do
+        card_1 = Card.new("What is the closest planet to the sun?", "Mercury", :STEM)
+        card_2 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_3 = Card.new("What is the 4th planet from the sun?", "Mars", :STEM)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
+
+        
+    end
+
+    it "will count the cards" do
+        card_1 = Card.new("What is the closest planet to the sun?", "Mercury", :STEM)
+        card_2 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_3 = Card.new("What is the 4th planet from the sun?", "Mars", :STEM)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
+        
+        expect(deck.count).to eq(3)
+    end
+
+    it "will show cards in a category" do
+        card_1 = Card.new("What is the closest planet to the sun?", "Mercury", :STEM)
+        card_2 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+        card_3 = Card.new("What is the 4th planet from the sun?", "Mars", :STEM)
+        cards = [card_1, card_2, card_3]
+        deck = Deck.new(cards)
+        cards_test = [card_1, card_3]
+
+        expect(deck.cards_in_category(:STEM)).to be eq(cards_test)
+    end
+end
